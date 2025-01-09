c60fc3af95bf40bd4ccbc6d01d95398db6fb6d7d
Proutum-Ann <526ALiang@frhsd.com>
2025-01-09 09:59:16 -0500

    added buttons and greeting div

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 userName = prompt("Hello! I don't seem to know you, do you mind telling me your name?")
-alert(`Hi there ${userName}, please get ready to have a rock, paper, scissors match!`)
+
+greetingText = ` <p>Hi there ${userName}, please get ready to have a rock, paper, scissors match! I won't be taking it easy on you!</p> `
+
+document.getElementById('greet').innerHTML = greetingText
+
+let userChoice = document.getElementById('usersPick')
+
+document.getElementById('userPlay').innerHTML = console.log(userChoice)
